1354846011114cc8005ad828bc819507f88abefc
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-08 01:37:19 -0600

    fixes

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = mongo.MongoClient("mongodb+srv://emiliogarrorangelpc:1A0b2C9d3E@cluster0.nkufong.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://emiliogarrorangelpc:1A0b2C9d3E@cluster0.nkufong.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 db = client.todo_db
 
--- routes/route.py
+++ routes/route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from models.todos import Todo
 from config.database import collection_name
-from schemas.schemas import list_serial
+from schema.schemas import list_serial
 
 router = APIRouter()
 
@@ -9,3 +9,11 @@ router = APIRouter()
 @router.get("/")
 async def get_todos():
     todos = list_serial(collection_name.find())
+    return todos
+
+
+@router.post("/")
+async def create_todo(todo: Todo):
+    todo = dict(todo)
+    collection_name.insert_one(todo)
+    return todo
