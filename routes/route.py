aa1f1a490f7f107401053933125ed2a6985d41ff
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-08 01:50:59 -0600

    added put endpoint

--- routes/route.py
+++ routes/route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from models.todos import Todo
 from config.database import collection_name
 from schema.schemas import list_serial
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -14,6 +15,13 @@ async def get_todos():
 
 @router.post("/")
 async def create_todo(todo: Todo):
-    todo = dict(todo)
-    collection_name.insert_one(todo)
-    return todo
+    tododict = dict(todo)
+    collection_name.insert_one(tododict)
+    return tododict
+
+
+@router.put("/{id}")
+async def update_todo(id: str, todo: Todo):
+    tododict = dict(todo)
+    collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": tododict})
+    return tododict
