af7015a6947eae072d4d36591ab2e3c89ac5fe12
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-07 23:28:48 -0600

    add router to fastapi

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from fastapi import FastAPI
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from routes.route import router
 
 app = FastAPI()
 
-uri = "mongodb+srv://emiliogarrorangelpc:1A0b2C9d3E@cluster0.nkufong.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(router)
